d34cecd21a0170b07d19c5336d95002cedd8002a
alazartegene <alazartegene@gmail.com>
2022-09-20 20:37:01 +0300

    other commit

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,21 @@
-# This manifest installs ngix and adds redirect page
+# Setup nginx server
 
-package {'nginx':
-  ensure => present,
-  name   => 'nginx',
+package { 'nginx':
+  ensure     => 'installed',
 }
 
-file {'/var/www/html/index.html':
-  ensure  => present,
-  path    => '/var/www/html/index.html',
-  content => 'Hello World!',
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
 }
 
-file_line { 'redirect_me':
-  ensure => present,
+file_line { 'aaaaa':
+  ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 service { 'nginx':
-  ensure     => running,
-  hasrestart => true,
-  require    => Package['nginx'],
-  subscribe  => File_line['redirect_me'],
+  ensure  => running,
+  require => Package['nginx'],
 }
